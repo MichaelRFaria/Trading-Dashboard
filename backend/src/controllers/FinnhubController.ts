42fe556675ffc4187b8fee3398668092b6e97be7
Michael Faria <michaelronaldfaria@gmail.com>
2026-06-11 13:43:05 +0100

    Implemented authentication guard using JWT cookie. Protected appropriate API routes.

--- backend/src/controllers/FinnhubController.ts
+++ backend/src/controllers/FinnhubController.ts
@@ -1,11 +1,14 @@
-import {Controller, Get, Query} from "@nestjs/common";
+import {Controller, Get, Query, UseGuards} from "@nestjs/common";
 import {FinnhubSymbolLookupDto} from "../dto/FinnhubSymbolLookupDto";
 import {FinnhubService} from "../services/FinnhubService";
+import {AuthGuard} from "../services/AuthGuard";
 
 @Controller("finnhub")
 export class FinnhubController {
     constructor(private finnHubService: FinnhubService) {
     }
+
+    @UseGuards(AuthGuard)
     @Get("symbol-lookup")
     async symbolLookup(@Query() finnHubSymbolLookupDto: FinnhubSymbolLookupDto) {
         return this.finnHubService.symbolLookup(finnHubSymbolLookupDto);
--- backend/src/controllers/WatchlistController.ts
+++ backend/src/controllers/WatchlistController.ts
@@ -1,17 +1,21 @@
-import {Body, Controller, Delete, Post} from "@nestjs/common";
+import {Body, Controller, Delete, Post, UseGuards} from "@nestjs/common";
 import {AddToWatchlistDto} from "../dto/AddToWatchlistDto";
 import {WatchlistService} from "../services/WatchlistService";
 import {DeleteFromWatchlistDto} from "../dto/DeleteFromWatchlistDto";
+import {AuthGuard} from "../services/AuthGuard";
 
 @Controller("watchlist")
 export class WatchlistController {
     constructor(private watchlistService: WatchlistService) {
     }
+
+    @UseGuards(AuthGuard)
     @Post("add")
     async add(@Body() addToWatchlistDto: AddToWatchlistDto) {
         return this.watchlistService.add(addToWatchlistDto);
     }
 
+    @UseGuards(AuthGuard)
     @Delete("delete")
     async delete(@Body() deleteFromWatchlistDto: DeleteFromWatchlistDto) {
         return this.watchlistService.delete(deleteFromWatchlistDto);
