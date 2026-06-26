2b5c0bd79bb08218fa3d0c045caa087a6886ebc1
Michael Faria <michaelronaldfaria@gmail.com>
2026-06-26 18:13:18 +0100

    Fixed oversight where derived values would only render once, before holdingsData loaded, causing empty data. Created basic calculation of largest holding.

--- frontend/src/components/Metrics.tsx
+++ frontend/src/components/Metrics.tsx
@@ -7,10 +7,13 @@ export default function Metrics({holdingsData}) {
     const [holdingsPrice, setHoldingsPrice] = useState<HoldingsPrice>({})
 
     useEffect(() => {
-        getPriceOfAllHoldings()
-    }, []);
+        if (holdingsData.length > 0) {
+            getPriceOfAllHoldings()
+        }
+    }, [holdingsData]);
 
     const getPriceOfAllHoldings = async () => {
+        console.log("getting price of holdings")
         const prices = await Promise.all(
             holdingsData.map(async (holding) => {
                 const request: StockSymbolLookupRequest = {
@@ -27,24 +30,36 @@ export default function Metrics({holdingsData}) {
 
         setHoldingsPrice(Object.fromEntries(prices))
     }
+
     const totalPortfolioValue = holdingsData.reduce((sum, holding) => {
         const price = holdingsPrice[holding.stock_symbol] ?? 0
 
         return sum + holding.quantity * price;
     }, 0);
 
+    const largestPosition = holdingsData.reduce((largest, holding) => {
+        const holdingValue = holding.quantity * (holdingsPrice[holding.stock_symbol] ?? 0)
 
-    useEffect(() => {
-        console.log(holdingsPrice);
-    }, [holdingsPrice]);
+        if (!largest || largest.value < holdingValue) {
+            return {
+                stock_symbol: holding.stock_symbol,
+                value: holdingValue
+            }
+        } else {
+            return largest
+        }
+    }, {
+        stock_symbol: null,
+        value: 0
+    })
 
     return (
         <>
             <div className="flex flex-col items-center">
-                <p>Portfolio Value:{totalPortfolioValue}</p>
+                <p>Portfolio Value: {totalPortfolioValue}</p>
                 <p>Today's Gain/Loss:</p>
                 <p>Total Gain/Loss:</p>
-                <p>Largest Position</p>
+                <p>Largest Position: {largestPosition.value} of {largestPosition.stock_symbol}</p>
                 <p>Number of Holdings: {holdingsData.length}</p>
             </div>
         </>
