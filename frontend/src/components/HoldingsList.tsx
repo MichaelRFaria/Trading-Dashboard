73e5b191b93db4e9e2dc887f41c9ce3dc789d7b9
Michael Faria <michaelronaldfaria@gmail.com>
2026-07-03 18:13:11 +0100

    Fixed TypeScript errors in HoldingsList.tsx.

--- frontend/src/components/HoldingsList.tsx
+++ frontend/src/components/HoldingsList.tsx
@@ -1,4 +1,6 @@
-export default function HoldingsList({holdingsData}) {
+import {HoldingsDataItem} from "@/src/types/trade";
+
+export default function HoldingsList({holdingsData}: {holdingsData: HoldingsDataItem[]}) {
     if (holdingsData.length <= 0) {
         return <p>Add some items to your holdings to view them</p>
     }
@@ -16,7 +18,7 @@ export default function HoldingsList({holdingsData}) {
                 </thead>
 
                 <tbody>
-                {holdingsData.map((item, index) =>
+                {holdingsData.map((item: HoldingsDataItem, index: number) =>
                     <tr>
                         <td>{index + 1}</td>
                         <td>{item.stock_symbol}</td>
