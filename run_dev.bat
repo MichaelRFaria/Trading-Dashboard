0ef5c4439c6b83c278ad6d8158ee1800a99041a9
Michael Faria <michaelronaldfaria@gmail.com>
2026-07-05 15:41:12 +0100

    Updated run dev script to include running the TypeScript compiler.

--- run_dev.bat
+++ run_dev.bat
@@ -5,3 +5,5 @@ docker compose up -d
 start "Backend" cmd /k "cd backend && npm run start:dev"
 
 start "Frontend" cmd /k "cd frontend && npm run dev"
+
+start "Typecheck" cmd /k "cd frontend && npm run typecheck"
